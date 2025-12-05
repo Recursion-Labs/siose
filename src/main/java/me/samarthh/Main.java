ad362be42a61879e5a295b7a302ab00363e5c778
samarth3301 <samarplayz1337@gmail.com>
2025-12-06 00:59:06 +0530

    Refactor Main class to extend JavaPlugin and implement command registration

--- src/main/java/me/samarthh/Main.java
+++ src/main/java/me/samarthh/Main.java
@@ -1,9 +1,32 @@
 package me.samarthh;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+import me.samarthh.commands.GetDataCommand;
+import me.samarthh.commands.RegisterCommand;
+import me.samarthh.commands.LoginCommand;
+import me.samarthh.managers.UserManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private UserManager userManager;
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin enabled!");
+
+        userManager = new UserManager();
+
+        // Register commands
+        this.getCommand("getdata").setExecutor(new GetDataCommand(userManager));
+        this.getCommand("register").setExecutor(new RegisterCommand(userManager));
+        this.getCommand("login").setExecutor(new LoginCommand(userManager));
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Plugin disabled!");
+        if (userManager != null) {
+            userManager.close();
+        }
     }
 }
