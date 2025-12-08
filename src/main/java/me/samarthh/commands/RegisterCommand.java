ace831fe1a05b27783bf7c98adf31c32534edd0c
samarth3301 <samarplayz1337@gmail.com>
2025-12-08 08:19:18 +0530

    Refactor RegisterCommand to remove API client usage and simplify registration message

--- src/main/java/me/samarthh/commands/RegisterCommand.java
+++ src/main/java/me/samarthh/commands/RegisterCommand.java
@@ -1,6 +1,5 @@
 package me.samarthh.commands;
 
-import me.samarthh.api.SioseApiClient;
 import me.samarthh.managers.UserManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,11 +11,9 @@ import java.util.UUID;
 public class RegisterCommand implements CommandExecutor {
 
     private final UserManager userManager;
-    private final SioseApiClient apiClient;
 
-    public RegisterCommand(UserManager userManager, String baseUrl) {
+    public RegisterCommand(UserManager userManager) {
         this.userManager = userManager;
-        this.apiClient = new SioseApiClient(baseUrl);
     }
 
     @Override
@@ -34,29 +31,7 @@ public class RegisterCommand implements CommandExecutor {
             return true;
         }
 
-        player.sendMessage("Initiating registration...");
-
-        // Use the API client for registration
-        apiClient.registerUser(uuid.toString(), player.getName())
-                .thenAccept(response -> {
-                    if (response.isSuccess()) {
-                        String message = "Registration initiated!";
-                        if (response.getRegistrationUrl() != null) {
-                            message += " Visit: " + response.getRegistrationUrl();
-                        }
-                        if (response.getRegistrationCode() != null) {
-                            message += " Code: " + response.getRegistrationCode();
-                        }
-                        message += " Then use /login <token> to complete authentication.";
-                        player.sendMessage(message);
-                    } else {
-                        player.sendMessage("Registration failed: " + response.getMessage());
-                    }
-                })
-                .exceptionally(throwable -> {
-                    player.sendMessage("Error during registration: " + throwable.getMessage());
-                    return null;
-                });
+        player.sendMessage("Please visit: https://frontend.com/register to register.");
 
         return true;
     }
